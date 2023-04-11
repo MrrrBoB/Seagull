ceaf4c0c22965803d4b51333ddfdde959d65f04f
Nate <Nathoward22@gmail.com>
2023-04-10 20:13:23 -0600

    Fixed the issue with UI not blocking raycasts

--- SeagullLite/Assets/Scripts/MoveToScreenPoint.cs
+++ SeagullLite/Assets/Scripts/MoveToScreenPoint.cs
@@ -17,13 +17,14 @@ public class MoveToScreenPoint : MonoBehaviour
     void Update()
     {
         rnder.enabled = Input.GetMouseButton(0);
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0)&& !EventSystem.current.IsPointerOverGameObject())
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, groundLayer))
             {
                 // move the object to the point where the ray hit the ground
                 Vector3 worldPos = hit.point;
+                Debug.Log(hit.collider.gameObject);
                 worldPos.y = yPos;
                 transform.position = worldPos;
             }
